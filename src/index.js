9627d6a9bdd3550c871bf97e4c8c2693690aa13f
sethdanny <naddulidaniel1994@gmail.com>
2024-07-08 18:14:29 +0300

    removed /api

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/api/auth', authRoutes);
+app.use('/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/organisations', organisationRoutes);
 
