70e1eac0a67e72f27b75ce690be9be180fe75408
sethdanny <naddulidaniel1994@gmail.com>
2024-07-08 17:03:27 +0300

    added welcome route

--- src/index.js
+++ src/index.js
@@ -20,15 +20,17 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/organisations', organisationRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the User Authentication and Organisation API');
+});
+
 app.use('*', (req, res, next) => {
     res.status(404).json({
         status: 'fail',
         message: 'Not found'
     })
 })
-app.get('/', (req, res) => {
-    res.json({message: 'Hello Welcome to User Authentication API'})
-})
+
 
     app.listen(PORT, () => {
         console.log(`Server is listening on port ${PORT}`);
